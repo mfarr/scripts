2b66be4002b8b28e01b845b4d5ea26b2f97a0f3e
Mike Farr <mike@noneleft.ca>
2024-04-01 06:03:52 -0400

    Set cert owner as variable

--- juno/cert-renew.sh
+++ juno/cert-renew.sh
@@ -8,11 +8,12 @@ cert_dir=/home/mike/Storage/server/cert
 domain_name=juno.fawn-alpha.ts.net
 passphrase_file=passphrase.txt
 syncthing_dir=../config/syncthing
+cert_owner=mike
 
 cd $cert_dir
 tailscale cert $domain_name
 openssl pkcs12 -export -out cert.pfx -inkey "${domain_name}.key" -in "${domain_name}.crt" -passout file:$passphrase_file
-chown mike:mike cert.pfx "${domain_name}.crt" "${domain_name}.key"
+chown $cert_owner:$cert_owner cert.pfx "${domain_name}.crt" "${domain_name}.key"
 cp "${domain_name}.crt" "${syncthing_dir}/https-cert.pem"
 cp "${domain_name}.key" "${syncthing_dir}/https-key.pem"
 docker-compose restart
