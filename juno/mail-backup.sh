082517ab611810d456eb585ca518201b70b54a5d
Mike Farr <mike@tokenize.ca>
2025-05-25 12:38:44 -0400

    Fix incorrect config and data directories

--- juno/mail-backup.sh
+++ juno/mail-backup.sh
@@ -1,14 +1,15 @@
 #! /bin/sh
 
 DATA_DIR="/mnt/backup/backup/Mike/Mail"
-CONFIG_FILE="/home/mike/.config/imap-backup/imap-backup.json"
+CONFIG_DIR="/home/mike/.config/imap-backup"
+CONFIG_FILE="imap-backup.json"
 HEALTHCHECK_UUID="3087eff1-86e1-4a21-b8d1-896631e3ddd4"
 
 docker run -it \
     --volume $DATA_DIR:/data \
-    --volume ./imap-backup/config:/config \
+    --volume $CONFIG_DIR:/config \
     --dns 8.8.8.8 ghcr.io/joeyates/imap-backup:latest \
-    imap-backup backup --config $CONFIG_FILE
+    imap-backup backup --config /config/$CONFIG_FILE
 
 exit_code=$?
 if [ $exit_code -ne 0 ]; then
