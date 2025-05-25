9ec6a513cc134e468719c21360779aba4f7d0ff7
Mike Farr <mike@tokenize.ca>
2025-05-25 12:36:10 -0400

    Add healthcheck ping to mail-backup

--- juno/mail-backup.sh
+++ juno/mail-backup.sh
@@ -2,9 +2,20 @@
 
 DATA_DIR="/mnt/backup/backup/Mike/Mail"
 CONFIG_FILE="/home/mike/.config/imap-backup/imap-backup.json"
+HEALTHCHECK_UUID="3087eff1-86e1-4a21-b8d1-896631e3ddd4"
 
 docker run -it \
     --volume $DATA_DIR:/data \
     --volume ./imap-backup/config:/config \
     --dns 8.8.8.8 ghcr.io/joeyates/imap-backup:latest \
     imap-backup backup --config $CONFIG_FILE
+
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+    echo "Error: Mail backup failed with exit code $exit_code"
+    curl --retry 3 https://hc-ping.com/$HEALTHCHECK_UUID/$exit_code
+    exit 1
+fi
+
+echo "Mail backup completed successfully"
+curl --retry 3 https://hc-ping.com/$HEALTHCHECK_UUID
